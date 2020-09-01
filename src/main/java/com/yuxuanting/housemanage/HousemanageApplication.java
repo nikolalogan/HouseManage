251952439102f7a0af15b1ce66071a5136da91cf
nikolalogan <44691689+nikolalogan@users.noreply.github.com>
2020-09-01 17:56:40 +0800

    构建项目

---
+++ src/main/java/com/yuxuanting/housemanage/HousemanageApplication.java
@@ -0,0 +1,13 @@
+package com.yuxuanting.housemanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousemanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousemanageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuxuanting/housemanage/HousemanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuxuanting.housemanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousemanageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
