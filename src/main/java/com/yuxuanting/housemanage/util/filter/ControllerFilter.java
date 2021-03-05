d67eacb8b343824c5b11a68d861c089c0c6b0160
nikolalogan <limao@20091223>
2021-03-05 17:30:21 +0800

    添加全局控制

---
+++ src/main/java/com/yuxuanting/housemanage/util/filter/ControllerFilter.java
@@ -0,0 +1,13 @@
+package com.yuxuanting.housemanage.util.filter;
+
+import com.nikolalogan.common.core.controller.filter.ResponseControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @author: yuxuanting
+ * @description:
+ * @date: 2021-03-05 17:21
+ */
+@RestControllerAdvice
+public class ControllerFilter extends ResponseControllerAdvice {
+}
